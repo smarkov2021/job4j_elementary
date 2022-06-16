4bc78d5059681011e383e153941fc4c7f9b81885
smarkov2020 <markovsv777@mail.ru>
2022-06-16 20:28:25 +0300

    Арифметические операции  [#243562]

---
+++ src/main/java/ru/job4j/Calculator.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int onePlusTwo = one + two;
+        System.out.println(onePlusTwo);
+    }
+}
---
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int onePlusTwo = one + two;
+        //System.out.println(onePlusTwo);
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimeTwo);
+    }
+}
