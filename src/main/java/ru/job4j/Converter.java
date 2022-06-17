da39fe0bd4519e9590d2b3501d76980abde48642
smarkov2020 <markovsv777@mail.ru>
2022-06-17 20:05:08 +0300

    Арифметические операции  [#243562]

---
+++ src/main/java/ru/job4j/Converter.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+public class Converter {
+
+    public static float rubleToEuro(float value) {
+        float rsl = value / 70;
+        return rsl;
+    }
+
+    public static float rubleToDollar(float value) {
+        float rsl = value / 68;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+
+        float dollars = Converter.rubleToDollar(140);
+        System.out.println("140 rubles are " + dollars + " dollars.");
+    }
+}
