7e652e7bf09ad98401f79e6369433915679ca6ec
smarkov2020 <markovsv777@mail.ru>
2022-06-17 20:14:34 +0300

    Идеальный вес [#243562]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,21 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        double rsl = (height - 100) * 1.15;
+        return rsl;
+    }
+
+    public static double womanWeight(short height) {
+        double rsl  = (height - 110) * 1.15;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        short height = 187;
+        double man = Fit.manWeight(height);
+        System.out.println("Man 187 is " + man);
+    }
+
+}
