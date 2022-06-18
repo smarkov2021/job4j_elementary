961809b33c35bf532fcd79bb989c416bf99ca8b9
smarkov2020 <markovsv777@mail.ru>
2022-06-18 18:54:20 +0300

    Расстояние [#243562]

---
+++ src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class Point {
+    public static double distance(int x1, int y1, int x2, int y2) {
+        //double res_abs = (double)(x2- x1);
+        //double res_ord = (double)(y2- y1);
+        double rsl = Math.pow((Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)), 0.5);
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double result = Point.distance(0, 0, 2, 0);
+        System.out.println("result (0, 0) to (2, 0) " + result);
+    }
+}
