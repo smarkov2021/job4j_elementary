9064f86cf1b5c50cd03f07b167bae098a1829bc3
smarkov2020 <markovsv777@mail.ru>
2022-06-18 19:04:17 +0300

    Площадь [#243562]

---
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class SqArea {
+    public static double square(double p, double k) {
+        double  h = p / (2 * (k + 1));
+        double l = h * k;
+        double rsl = l * h;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        double result1 = SqArea.square(6, 2);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+    }
+}
