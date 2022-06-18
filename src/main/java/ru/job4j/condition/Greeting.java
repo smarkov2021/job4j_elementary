5ffef3562e6490188348b50dc5ef76e554f56207
smarkov2020 <markovsv777@mail.ru>
2022-06-18 19:21:31 +0300

    Площадь треугольника [#243562]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+    String idea = "I like Java!";
+    System.out.println(idea);
+    idea = idea + "But I am a newbie.";
+    Int year = 2022;
+    idea = idea + year;
+    System.out.println(idea);
+    }
+}
