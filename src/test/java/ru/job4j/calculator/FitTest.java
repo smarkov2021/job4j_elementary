26dc3569b2e93134df01357844bb5d53b0b5af0c
smarkov2020 <markovsv777@mail.ru>
2022-06-19 20:08:05 +0300

    Тест идеальный вес

---
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FitTest {
+
+    @Test
+    public void whenMan180Then92() {
+        short in = 180;
+        double expected = 92;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenMan189Then10235() {
+        short in = 189;
+        double expected = 102.35;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWoman170Then69() {
+        short in = 170;
+        double expected = 69;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
