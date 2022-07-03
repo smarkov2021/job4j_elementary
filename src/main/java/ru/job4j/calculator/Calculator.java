774d9803632372443f8636fcc6efe3ead0722fbb
smarkov2020 <markovsv777@mail.ru>
2022-07-03 15:56:16 +0300

    коммит Calculator

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -6,7 +6,6 @@ public class Calculator {
         int one = 1;
         int two = 2;
         int onePlusTwo = one + two;
-        //System.out.println(onePlusTwo);
         int six = 6;
         int four = 4;
         int five = 5;
