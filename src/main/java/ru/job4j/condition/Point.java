36d0e66a66bb5f2551e30bb350a95cfc84577734
smarkov2020 <markovsv777@mail.ru>
2022-07-05 17:41:02 +0300

    коммит Point

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -8,6 +8,6 @@ public class Point {
 
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0)  " + result);
+        System.out.println("result (0, 0) to (2, 0) " + result);
     }
 }
