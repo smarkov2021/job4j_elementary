0c6b17f83d3de6746495ff5d348a16a6b2b796d8
smarkov2020 <markovsv777@mail.ru>
2022-07-05 17:10:39 +0300

    коммит ArgMethod

---
+++ src/main/java/ru/job4j/ArgMethod.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class ArgMethod {
+
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", age = " + age);
+    }
+
+    public static void main(String[] args) {
+        String name = "Job4j";
+        int age = 6;
+        ArgMethod.hello(name, age);
+    }
+}
