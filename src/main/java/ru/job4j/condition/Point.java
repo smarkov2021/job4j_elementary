255e160e3f31e79a8bcdc9442c2f24a493725923
smarkov2020 <markovsv777@mail.ru>
2022-07-08 06:03:16 +0300

    коммит Converter

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.pow((Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)), 0.5);
+        double rsl = Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5);
         return rsl;
     }
 
