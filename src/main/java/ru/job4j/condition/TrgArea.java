bf45cd4d53bc116824ad7f8869bd82ba68a70859
smarkov2020 <markovsv777@mail.ru>
2022-07-10 18:41:22 +0300

    коммит TrgArea

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -10,6 +10,7 @@ public class TrgArea {
     public static void main(String[] args) {
         double rsl = TrgArea.area(2, 2, 2);
         System.out.println("area (2, 2, 2) = " + rsl);
+
     }
 
 }
