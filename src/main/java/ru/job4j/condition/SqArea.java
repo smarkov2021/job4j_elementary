47f611307207b9adc230af61e905dc0ed41f317b
smarkov2020 <markovsv777@mail.ru>
2022-07-10 18:30:15 +0300

    коммит SqArea

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -9,7 +9,6 @@ public class SqArea {
     }
 
     public static void main(String[] args) {
-
         double result1 = SqArea.square(6, 2);
         System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
     }
