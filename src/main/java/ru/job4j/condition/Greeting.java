7261aaff2932147e1d7b02cb46ae5d79972d316a
smarkov2020 <markovsv777@mail.ru>
2022-07-13 08:18:07 +0300

    коммит Greeting

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -2,11 +2,11 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-    String idea = "I like Java!";
-    System.out.println(idea);
-    idea = idea + "But I am a newbie.";
-    int year = 2022;
-    idea = idea + year;
-    System.out.println(idea);
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea = idea + "But I am a newbie.";
+        int year = 2022;
+        idea = idea + year;
+        System.out.println(idea);
     }
 }
