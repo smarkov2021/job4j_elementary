e7fa569ded6177211db0e938a040cfa3708de84d
smarkov2020 <markovsv777@mail.ru>
2022-07-13 08:13:01 +0300

    коммит TrgArea

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -2,9 +2,8 @@ package ru.job4j.condition;
 
 public class TrgArea {
     public static double area(double a, double b, double c) {
-        double per = a + b + c;
-        double rsl = Math.sqrt(per * (per - a) * (per - b) * (per - c));
-        return rsl;
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
     public static void main(String[] args) {
