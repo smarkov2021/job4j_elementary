e8ec9c6085b307a9f2f06f1fd182e5cb3e762ea0
smarkov2020 <markovsv777@mail.ru>
2022-07-16 19:59:21 +0300

    коммит MathCalculator

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -9,6 +9,15 @@ public class MathCalculator {
                 + MathFunction.multiply(first, second);
     }
 
+    public static double sumAllOperations(double first, double second) {
+        return MathFunction.sum(first, second)
+                + MathFunction.multiply(first, second) + MathFunction.division(first, second) + MathFunction.difference(first, second);
+    }
+
+    public static double divAndDiff(double first, double second) {
+        return MathFunction.division(first, second) + MathFunction.difference(first, second);
+    }
+
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
     }
