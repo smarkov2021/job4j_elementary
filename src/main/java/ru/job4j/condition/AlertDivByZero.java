0efce1693823100766e13f76ca4cb865636939a3
smarkov2020 <markovsv777@mail.ru>
2022-07-17 11:52:24 +0300

    коммит AlertDivByZero

---
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class AlertDivByZero {
+    public static void main(String[] args) {
+        possibleDiv(4);
+        possibleDiv(0);
+        possibleDiv(-10);
+    }
+
+    public static void possibleDiv(int number) {
+        if (number == 0) {
+            System.out.println("Could not div by 0.");
+        }
+        if (number < 0) {
+            System.out.println("This is negative number.");
+        }
+    }
+}
