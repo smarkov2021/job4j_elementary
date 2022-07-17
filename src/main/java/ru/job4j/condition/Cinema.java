5e406104fae87c1fd522ffec412de3b6e1efa430
smarkov2020 <markovsv777@mail.ru>
2022-07-17 11:57:29 +0300

    коммит Cinema

---
+++ src/main/java/ru/job4j/condition/Cinema.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Cinema {
+    public static void access(int age) {
+        System.out.println("The age of the customer is : " + age);
+        if (age >= 18) {
+            System.out.println("Welcome to the cinema.");
+        } else {
+            System.out.println("It is not for you.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Cinema.access(21);
+        Cinema.access(16);
+
+    }
+}
