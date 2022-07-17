98f0dde968af51daaac432b198555d2c32006d0f
smarkov2020 <markovsv777@mail.ru>
2022-07-17 15:04:15 +0300

    коммит Cinema

---
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String rsl = DummyBot.answer("Привет, Бот.");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Пока.");
+        System.out.println(rsl);
+    }
+}
---
+++ src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyBotTest {
+    @Test
+    public void whenGreetBot() {
+        String in = "Привет, Бот.";
+        String result = DummyBot.answer(in);
+        String expected = "Привет, умник.";
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenByeBot() {
+        String in = "Пока.";
+        String result = DummyBot.answer(in);
+        String expected = "До скорой встречи.";
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        String in = "Сколько будет 2 + 2?";
+        String result = DummyBot.answer(in);
+        String expected = "Это ставит меня в тупик. Задайте другой вопрос.";
+        Assert.assertEquals(expected, result);
+    }
+}
