f410a2acd7f9d9c08b99c513912efbabb6129af3
smarkov2020 <markovsv777@mail.ru>
2022-07-23 17:48:55 +0300

    коммит LogicNot

---
+++ src/main/java/ru/job4j/condition/LogicNot.java
@@ -0,0 +1,29 @@
+package ru.job4j.condition;
+
+public class LogicNot {
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static boolean isPositive(int num) {
+        return num > 0;
+    }
+
+    public static boolean notEven(int num) {
+        return !isEven(num);
+    }
+
+    public static boolean notPositive(int num) {
+        return !isPositive(num);
+    }
+
+    public static boolean notEvenAndPositive(int num) {
+        return !isEven(num) && isPositive(num);
+    }
+
+    public static boolean evenOrNotPositive(int num) {
+        return isEven(num) || !isPositive(num);
+    }
+
+}
