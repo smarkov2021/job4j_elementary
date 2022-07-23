008fadeb5911721563c97d4fd0a62582df74972d
smarkov2020 <markovsv777@mail.ru>
2022-07-23 17:43:59 +0300

    коммит Club

---
+++ src/main/java/ru/job4j/condition/Club.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Club {
+    public static void permission(boolean hasMoney, boolean beFriend) {
+        if (hasMoney || beFriend) {
+            System.out.println("I can go to the club.");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+
+    public static void main(String[] args) {
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
+    }
+}
