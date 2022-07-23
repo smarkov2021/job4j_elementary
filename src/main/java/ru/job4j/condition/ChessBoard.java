1933af89bb1edfe21ddd7a25fe4af95c28a8a4dd
smarkov2020 <markovsv777@mail.ru>
2022-07-23 18:55:59 +0300

    коммит ChessBoard

---
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+
+    public static int way(int x1, int y1, int x2, int y2) {
+        int rsl = 0;
+        if (isValid(x1) && isValid(x2)
+                && isValid(y1) && isValid(y2)) {
+            if (Math.abs(x1 - x2) == Math.abs(y1 - y2)) {
+                rsl = Math.abs(x2 - x1);
+            }
+        }
+        return rsl;
+    }
+
+    private static boolean isValid(int coordinate) {
+        return coordinate >= 0 && coordinate <= 7;
+    }
+}
---
+++ src/test/java/ru/job4j/condition/ChessBoardTest.java
@@ -0,0 +1,127 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ChessBoardTest {
+    @Test
+    public void wayIs5() {
+        int x1 = 6;
+        int y1 = 7;
+        int x2 = 1;
+        int y2 = 2;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 5;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void wayIs7() {
+        int x1 = 7;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 7;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 7;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void wayIs0() {
+        int x1 = 2;
+        int y1 = 6;
+        int x2 = 4;
+        int y2 = 1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenX1isMinus1ThenWayIs0() {
+        int x1 = -1;
+        int y1 = 6;
+        int x2 = 4;
+        int y2 = 1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenY1isMinus1ThenWayIs0() {
+        int x1 = 2;
+        int y1 = -1;
+        int x2 = 4;
+        int y2 = 1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenX2isMinus1ThenWayIs0() {
+        int x1 = 2;
+        int y1 = 6;
+        int x2 = -1;
+        int y2 = 1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenY2isMinus1ThenWayIs0() {
+        int x1 = 2;
+        int y1 = 6;
+        int x2 = 4;
+        int y2 = -1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenX1isGreater7ThenWayIs0() {
+        int x1 = 10;
+        int y1 = 6;
+        int x2 = 4;
+        int y2 = 1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenY1isGreater7ThenWayIs0() {
+        int x1 = 2;
+        int y1 = 10;
+        int x2 = 4;
+        int y2 = 1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenX2isGreater7ThenWayIs0() {
+        int x1 = 2;
+        int y1 = 6;
+        int x2 = 10;
+        int y2 = 1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenY2isGreater7ThenWayIs0() {
+        int x1 = 2;
+        int y1 = 6;
+        int x2 = 4;
+        int y2 = 10;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+}
