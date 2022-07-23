c7ae9fbe83281705d922d388cb446ea69180aeec
smarkov2020 <markovsv777@mail.ru>
2022-07-23 17:33:18 +0300

    коммит Games

---
+++ src/main/java/ru/job4j/condition/Games.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Games {
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        if (allowByParent && hasMoney) {
+            System.out.println("I can go to the computer club.");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Games.permission(true, true);
+        Games.permission(true, false);
+        Games.permission(false, true);
+        Games.permission(false, false);
+    }
+}
