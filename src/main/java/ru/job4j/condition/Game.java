ccf4ce6d3a010edf59000d3c03dea1b4df6113bf
smarkov2020 <markovsv777@mail.ru>
2022-07-23 17:03:31 +0300

    коммит Game

--- src/main/java/ru/job4j/condition/Game.java
+++ src/main/java/ru/job4j/condition/Game.java
@@ -2,13 +2,13 @@ package ru.job4j.condition;
 
 public class Game {
     public static void menu(String name) {
-        if ("super mario".equals(name)) { /* сравниваем переменную name с "super mario" */
+        if ("super mario".equals(name)) {
             System.out.println("Start - super mario");
         }
-        if ("tanks".equals(name)) { /* Сравнить переменную name с "tanks" */
+        if ("tanks".equals(name)) {
             System.out.println("Start - tanks");
         }
-        if ("tetris".equals(name)) { /* Сравнить переменную name с "tetris" */
+        if ("tetris".equals(name)) {
             System.out.println("Start - tetris");
         }
     }
