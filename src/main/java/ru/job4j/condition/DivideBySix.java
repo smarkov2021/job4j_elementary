9b00cd943920c34d56f9105a8cbd3f4ef8819a1b
smarkov2020 <markovsv777@mail.ru>
2022-07-24 15:10:46 +0300

    коммит checkNumber

--- src/main/java/ru/job4j/condition/DivideBySix.java
+++ src/main/java/ru/job4j/condition/DivideBySix.java
@@ -5,9 +5,9 @@ public class DivideBySix {
         String rsl;
         if (number % 3 == 0 && number % 2 == 0) {
             rsl = "Исходное число делится на 6.";
-        } else if (number % 3 == 0 && number % 2 != 0) {
+        } else if (number % 3 == 0) {
             rsl = "Исходное число делится на 3, но не является четным.";
-        } else if (number % 3 != 0 && number % 2 == 0) {
+        } else if (number % 2 == 0) {
             rsl = "Исходное число не делится на 3, но является четным.";
         } else {
             rsl = "Исходное число не делится на 3 и не является четным.";
