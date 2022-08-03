9e8cd79376458619e0cdb91991acb26b86a630cf
smarkov2020 <markovsv777@mail.ru>
2022-08-03 07:34:42 +0300

    коммит Counter

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,15 +3,18 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (int value = start; value <= finish; value++) {
-            sum = sum + value;
+        for (int i = start; i <= finish; i++) {
+            sum = sum + i;
         }
         return sum;
     }
 
-    public static void main(String[] args) {
-        System.out.println(sum(0, 10));
-        System.out.println(sum(3, 8));
-        System.out.println(sum(1, 1));
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            int value = i % 2 == 0 ? i : 0;
+            sum = sum + value;
+        }
+        return sum;
     }
 }
---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CounterTest {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+}
