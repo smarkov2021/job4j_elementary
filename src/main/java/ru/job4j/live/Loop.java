5563546f3f285cdf61b7b88115b6dca552c4145d
smarkov2020 <markovsv777@mail.ru>
2022-08-07 18:40:04 +0300

    коммит Loop

---
+++ src/main/java/ru/job4j/live/Loop.java
@@ -0,0 +1,9 @@
+package ru.job4j.live;
+
+public class Loop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
+    }
+}
