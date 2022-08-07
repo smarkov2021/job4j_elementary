7d63c9f19ca593798981dab01d4399de21178e00
smarkov2020 <markovsv777@mail.ru>
2022-08-07 17:58:50 +0300

    коммит Factorial

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -3,8 +3,8 @@ package ru.job4j.loop;
 public class Factorial {
     public static int calc(int n) {
         int result = 1;
-        for (int i = 1; i <= n; i++) {
-            result = result * i;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
         }
         return result;
     }
