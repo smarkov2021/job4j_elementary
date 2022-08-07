0bb830efdb0fefa070ee59e9b894915142f30b97
smarkov2020 <markovsv777@mail.ru>
2022-08-07 18:51:36 +0300

    коммит ArrayDefinition

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
