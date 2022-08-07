1433d65d242c738d74fc2919b817288c7b145ec8
smarkov2020 <markovsv777@mail.ru>
2022-08-07 18:17:02 +0300

    коммит Fitness

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -4,9 +4,9 @@ public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
         while (ivan <= nik) {
-        ivan = ivan * 3;
-        nik = nik * 2;
-        month++;
+            ivan *= 3;
+            nik *= 2;
+            month++;
         }
         return month;
     }
