be4f48755afce17886744311da265ea0cecc1178
smarkov2020 <markovsv777@mail.ru>
2022-08-08 19:25:41 +0300

    коммит ArrayLoop(foreach)

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -6,8 +6,8 @@ public class ArrayLoop {
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = index * 2 + 3;
         }
-        for (int index = 0; index < numbers.length; index++) {
-            System.out.println(numbers[index]);
+        for (int number : numbers) {
+            System.out.println(number);
         }
     }
 }
