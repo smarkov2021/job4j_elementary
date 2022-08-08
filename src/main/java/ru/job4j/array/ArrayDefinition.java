25aad4778aa2e41fec732caf3027c1f4f48a5296
smarkov2020 <markovsv777@mail.ru>
2022-08-08 19:15:24 +0300

    коммит ArrayDefinition

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Petr";
+        names[1] = "Alex";
+        names[2] = "James";
+        names[3] = "Tom";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
