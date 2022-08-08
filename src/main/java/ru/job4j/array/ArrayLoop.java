7bf2eb200254a5ed684fc83d58c4b370a9ff094f
smarkov2020 <markovsv777@mail.ru>
2022-08-08 19:22:03 +0300

    коммит ArrayLoop

---
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class ArrayLoop {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        for (int index = 0; index < numbers.length; index++) {
+            numbers[index] = index * 2 + 3;
+        }
+        for (int index = 0; index < numbers.length; index++) {
+            System.out.println(numbers[index]);
+        }
+    }
+}
