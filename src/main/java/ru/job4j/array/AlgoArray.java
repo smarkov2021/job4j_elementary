30c189c28dabbb41a0b4fa4f3c18f104443601cb
smarkov2020 <markovsv777@mail.ru>
2022-08-10 19:04:23 +0300

    коммит AlgoArray

---
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class AlgoArray {
+    public static void main(String[] args) {
+        int[] array = new int[] {5, 3, 2, 1, 4};
+        int temp = array[0];
+        array[0] = array[3];
+        array[3] = temp;
+        temp = array[1];
+        array[1] = array[2];
+        array[2] = temp;
+        temp = array[3];
+        array[3] = array[4];
+        array[4] = temp;
+
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+    }
+}
