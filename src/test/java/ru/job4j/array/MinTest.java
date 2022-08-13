decf70b7d519b2c47cd99949ba7a6fbf94108988
smarkov2020 <markovsv777@mail.ru>
2022-08-13 13:38:53 +0300

    коммит findMin

---
+++ src/test/java/ru/job4j/array/MinTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MinTest {
+    @Test
+    public void whenFirstMin() {
+        int[] array = new int[] {0, 5, 10};
+        int result = Min.findMin(array);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenLastMin() {
+        int[] array = new int[] {10, 5, 3};
+        int result = Min.findMin(array);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMiddleMin() {
+        int[] array = new int[] {10, 2, 5};
+        int result = Min.findMin(array);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+}
