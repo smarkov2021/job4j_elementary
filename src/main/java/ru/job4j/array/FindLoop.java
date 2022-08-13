892cdebd4a5247ca5fdb7124565b1f0f5dadeea2
smarkov2020 <markovsv777@mail.ru>
2022-08-13 13:08:43 +0300

    коммит FindLoop

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -11,4 +11,15 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int index = start; index < finish; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
 }
--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -21,4 +21,37 @@ public class FindLoopTest {
         int expected = -1;
         assertThat(result).isEqualTo(expected);
     }
-}
+
+        @Test
+        public void whenDiapasonHas2Then3() {
+            int[] data = new int[] {5, 2, 10, 2, 4};
+            int el = 2;
+            int start = 2;
+            int finish = 4;
+            int result = FindLoop.indexInRange(data, el, start, finish);
+            int expected = 3;
+            assertThat(result).isEqualTo(expected);
+        }
+
+         @Test
+         public void whenDiapasonHasNot4ThenMinus1() {
+             int[] data = new int[] {5, 2, 10, 2, 4};
+             int el = 4;
+             int start = 2;
+             int finish = 3;
+             int result = FindLoop.indexInRange(data, el, start, finish);
+             int expected = -1;
+             assertThat(result).isEqualTo(expected);
+        }
+
+        @Test
+        public void whenDiapasonHasNot5ThenMinus1() {
+            int[] data = new int[] {5, 2, 10, 2, 4};
+            int el = 5;
+            int start = 1;
+            int finish = 4;
+            int result = FindLoop.indexInRange(data, el, start, finish);
+            int expected = -1;
+            assertThat(result).isEqualTo(expected);
+    }
+    }
