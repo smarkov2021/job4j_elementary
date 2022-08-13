b5a781deaef8bbefe798022d6b326d91321780d8
smarkov2020 <markovsv777@mail.ru>
2022-08-13 12:10:37 +0300

    коммит Check

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -4,7 +4,7 @@ public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
         for (int index = 1; index < data.length; index++) {
-            if (data[index] != data[index - 1]) {
+            if (data[index] != data[0]) {
                 result = false;
                 break;
             }
