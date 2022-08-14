0c9435853ed5c8415c8ce293c1798adf0c221402
smarkov2020 <markovsv777@mail.ru>
2022-08-14 16:37:42 +0300

    коммит SkipNegative

---
+++ src/main/java/ru/job4j/array/SkipNegative.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class SkipNegative {
+    public static int[][] skip(int[][] array) {
+        for (int row = 0; row < array.length; row++) {
+            for (int cell = 0; cell < array[row].length; cell++) {
+                if (array[row][cell] < 0) {
+                    array[row][cell] = 0;
+                }
+            }
+        }
+    return array;
+    }
+}
---
+++ src/test/java/ru/job4j/array/SkipNegativeTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SkipNegativeTest {
+    @Test
+    public void whenArrayRowAndColumnEquals() {
+        int[][] array = {
+                {1, -2},
+                {1, 2}
+        };
+        int[][] result = SkipNegative.skip(array);
+        int[][] expected = {
+                {1, 0},
+                {1, 2}
+        };
+        assertThat(result).isDeepEqualTo(expected);
+    }
+
+    @Test
+    public void whenArrayRow4AndColumnNotEquals() {
+        int[][] array = {
+                {1, -2},
+                {1, 2, -3},
+                {1, -2, 3, -4},
+                {1, 2, -3, -4, 5}
+        };
+        int[][] result = SkipNegative.skip(array);
+        int[][] expected = {
+                {1, 0},
+                {1, 2, 0},
+                {1, 0, 3, 0},
+                {1, 2, 0, 0, 5}
+        };
+        assertThat(result).isDeepEqualTo(expected);
+    }
+}
