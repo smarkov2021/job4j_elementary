00ef1b2baad70d7df305c8048aca596ea4eebab2
smarkov2020 <markovsv777@mail.ru>
2022-08-14 15:59:09 +0300

    коммит Min

--- src/main/java/ru/job4j/array/Min.java
+++ src/main/java/ru/job4j/array/Min.java
@@ -3,8 +3,10 @@ package ru.job4j.array;
 public class Min {
     public static int findMin(int[] array) {
         int min = array[0];
-        for (int index = 0; index < array.length; index++) {
-            min = array[index] < min ? array[index] : min;
+        for (int index = 1; index < array.length; index++) {
+             if (array[index] < min) {
+                 min = array[index];
+             }
         }
         return min;
     }
