6767eaa458cb70ac7fe2097945a7b71b1ba179df
smarkov2020 <markovsv777@mail.ru>
2022-08-14 15:54:30 +0300

    коммит EndsWith

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -4,9 +4,7 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
         for (int index = 0; index < post.length; index++) {
-            int currentIndexForWord = word.length - index - 1;
-            int currentIndexForPost = post.length - index - 1;
-            if (post[currentIndexForPost] != word[currentIndexForWord]) {
+            if (post[post.length - index - 1] != word[word.length - index - 1]) {
                 result = false;
                 break;
                 }
