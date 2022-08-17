14614e1ad8bb76a004e027ab7d399521920c9a92
smarkov2020 <markovsv777@mail.ru>
2022-08-17 19:24:16 +0300

    коммит Defragment

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -4,7 +4,7 @@ public class Defragment {
     public static String[] compress(String[] array) {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
-                for (int i = index; i < array.length; i++) {
+                for (int i = index + 1; i < array.length; i++) {
                     if (array[i] != null) {
                         array[index] = array[i];
                         array[i] = null;
