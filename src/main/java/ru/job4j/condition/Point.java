fa34cfc9e5a7e17cdb27551c0fee84c1428ab91c
smarkov <markovsv777@mail.ru>
2022-10-02 15:59:27 +0300

    Коммит Point3D

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -5,22 +5,10 @@ import static java.lang.Math.pow;
 
 public class Point {
 
-    /**
-     * Это поле объекта. Оно доступно только конкретному объекту.
-     */
     private int x;
-
-    /**
-     * И это поле объекта. Оно доступно только конкретному объекту.
-     */
     private int y;
     private int z;
 
-    /**
-     * Конструктор, который принимает начальное состояние объекта "точка"
-     * @param first координата x
-     * @param second координата y
-     */
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
