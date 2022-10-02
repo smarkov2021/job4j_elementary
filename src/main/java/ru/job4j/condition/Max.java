964c0b17f3a22fcd5d55826c0fb118c49c848e3a
smarkov <markovsv777@mail.ru>
2022-10-02 10:39:31 +0300

    Коммит Max

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,7 +1,15 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        return  left > right ? left : right;
+    public int max(int first, int second) {
+        return  first > second ? first : second;
+    }
+
+    public int max(int first, int second, int third) {
+        return  third > max(first, second) ? third : max(first, second);
+    }
+
+    public int max(int first, int second, int third, int fourth) {
+        return  fourth > max(first, second, third) ? fourth : max(first, second, third);
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,10 +6,34 @@ import org.junit.Test;
 public class MaxTest {
     @Test
     public void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+        int first = 1;
+        int second = 2;
+        Max firstTest = new Max();
+        int result = firstTest.max(first, second);
         int expected = 2;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax1To2To3Then3() {
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        Max secondTest = new Max();
+        int result = secondTest.max(first, second, third);
+        int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax1To2To2To7Then7() {
+        int first = 1;
+        int second = 2;
+        int third = 2;
+        int fourth = 7;
+        Max secondTest = new Max();
+        int result = secondTest.max(first, second, third, fourth);
+        int expected = 7;
         Assert.assertEquals(result, expected);
     }
 }
