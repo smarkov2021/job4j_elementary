b1821a32bd918e7a6ad25d3350cf907a5ea6292b
smarkov <markovsv777@mail.ru>
2022-11-08 19:25:02 +0300

    Коммит StringRunner

---
+++ src/main/java/ru/job4j/condition/StringRunner.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class StringRunner {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += " But I am a newbie. ";
+        int year = 2022;
+        idea += year;
+        System.out.println(idea);
+    }
+}
